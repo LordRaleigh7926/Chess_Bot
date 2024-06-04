bd90fb80c37bfee4fe2e159d5627d23582344592
LordRaleigh7926 <ankushroy7926@gmail.com>
2024-06-04 23:07:42 +0530

    error correction

--- Assets/legal.cs
+++ Assets/legal.cs
@@ -4,11 +4,7 @@ using System;
 public static class LegalMoves{
 
 
-    void checkLegalMoves(Pieces piece){
-
-        if (piece == Pieces.None) {
-            Debug.LogError("CheckLegalMoves got Pieces.None as a piece in parameter");
-        }
+    public static void checkLegalMoves(Pieces piece){
 
         int color = piece & (Pieces.White | Pieces.Black);
         int type = piece & ~(Pieces.White | Pieces.Black);
